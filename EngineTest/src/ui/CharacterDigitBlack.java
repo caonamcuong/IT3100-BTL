ee01536aa7468cd202dc041744f9131f6137e673
caonamcuong <caonamcuonga1k19hht@gmail.com>
2024-06-04 08:17:15 +0700

    Revert "last change"
    This reverts commit efdebcdce08e84a4c7d1aedcdedf19244f220982.

--- EngineTest/src/ui/CharacterDigitBlack.java
+++ EngineTest/src/ui/CharacterDigitBlack.java
@@ -12,154 +12,154 @@ public class CharacterDigitBlack extends BasicObject {
 	private static final TreeMap<String, BasicSprite> state_machine = new TreeMap<String, BasicSprite>() {{
 		put("null", null);
 		put("clock", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(0),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("0", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("1", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*2),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("2", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*3),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("3", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*4),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("4", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*5),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("5", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*6),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("6", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*7),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("7", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*8),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("8", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*9),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("9", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*10),
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put(":", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(0),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("0b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("1b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*2),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("2b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*3),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("3b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*4),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("4b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*5),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("5b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*6),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("6b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*7),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("7b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*8),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("8b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*9),
 				Arrays.asList(40),
 				Arrays.asList(40),
 				Arrays.asList(40)
 			));
 		put("9b", new BasicSprite (
-				"font_black.png",
+				"bin/font_black.png",
 				Arrays.asList(40*10),
 				Arrays.asList(40),
 				Arrays.asList(40),
